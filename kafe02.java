c3aaa39e1e227ffd4b9837648988b2ba296e642e
achmadanval <anvalalan@gmail.com>
2024-09-11 10:03:18 +0700

    anval jobsheeet-3

---
+++ kafe02.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class kafe02 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean keanggotaan;
+        int jmlKopi, jmlTeh, jmlRoti;
+        double hargaKopi = 12000.0, hargaTEh = 7000.0, hargaRoti = 20000.0;
+        float diskon = 10 / 100;
+        System.out.print("Masukkan keanggotaan (ture/false): ");
+        keanggotaan = input.nextBoolean();
+        System.out.print("Masukkan jumlah pembelian kopi: ");
+        jmlKopi = input.nextInt();
+        System.out.print("Masukkan jumlah pembeliat teh: ");
+        jmlTeh = input.nextInt();
+        System.out.print("Masukkan jumlah pembelian roti: ");
+        jmlRoti = input.nextInt();
+        double totalHarga = (jmlKopi * hargaKopi) + (jmlRoti * hargaRoti) + (jmlTeh * hargaTEh);
+        double nominalBayar = totalHarga - (diskon * totalHarga);
+        System.out.println("Keanggotaan pelanggan " + keanggotaan);
+        System.out.println("Item pembelian " + jmlKopi + "kopi, " + jmlRoti + "Roti, " + jmlTeh + "Teh ");
+        System.out.println("Nominal bayar Rp " + nominalBayar);
+
+        
+    }
+}
